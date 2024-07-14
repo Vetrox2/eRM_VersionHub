c053e670b263327627ab3232c990c96c9724455c
Vetrox2 <mateuszmulka@spoko.pl>
2024-07-14 11:57:34 +0200

    test(TagService): unit tests

---
+++ eRM_VersionHub/Tests/Tests/TagServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using eRM_VersionHub.Models;
+using eRM_VersionHub.Services;
+using eRM_VersionHub_Tester.Services;
+
+namespace eRM_VersionHub_Tester.Tests
+{
+    public class TagServiceTests : IAsyncLifetime
+    {
+        private readonly TagService _tagService;
+        private string appsPath, appJson, internalPath, externalPath;
+
+        public TagServiceTests()
+        {
+            _tagService = new TagService();
+        }
+
+        public Task InitializeAsync()
+        {
+            (appsPath, appJson, internalPath, externalPath) = FileStructureGenerator.GenerateFileStructure();
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            FileStructureGenerator.DeleteFileStructure();
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public void SetTag_ShouldReturnSuccessResponse()
+        {
+            // Arrange
+            var settings = new MyAppSettings()
+            {
+                ApplicationConfigFile = appJson,
+                AppsPath = appsPath,
+                ExternalPackagesPath = externalPath,
+                InternalPackagesPath = internalPath
+            };
+            var appID = "app2";
+            var versionID = "0.1";
+            var newTag = "testTag";
+
+            // Act
+            var response = _tagService.SetTag(settings, appID, versionID, newTag);
+
+            // Assert
+            Assert.True(response.Success);
+            Assert.Equal("Internal modules version modified: 2\nPublished modules version modified: 1", response.Data);
+        }
+
+        [Fact]
+        public void SetTag_ShouldReturnAppNotFound()
+        {
+            // Arrange
+            var settings = new MyAppSettings()
+            {
+                ApplicationConfigFile = appJson,
+                AppsPath = appsPath,
+                ExternalPackagesPath = externalPath,
+                InternalPackagesPath = internalPath
+            };
+            var appID = "app4";
+            var versionID = "0.1";
+            var newTag = "testTag";
+
+            // Act
+            var response = _tagService.SetTag(settings, appID, versionID, newTag);
+
+            // Assert
+            Assert.False(response.Success);
+            Assert.Equal("App not found", response.Errors[0]);
+        }
+
+        [Fact]
+        public void SetTag_ShouldReturnNoneVersionModified()
+        {
+            // Arrange
+            var settings = new MyAppSettings()
+            {
+                ApplicationConfigFile = appJson,
+                AppsPath = appsPath,
+                ExternalPackagesPath = externalPath,
+                InternalPackagesPath = internalPath
+            };
+            var appID = "app2";
+            var versionID = "0.4";
+            var newTag = "testTag";
+
+            // Act
+            var response = _tagService.SetTag(settings, appID, versionID, newTag);
+
+            // Assert
+            Assert.False(response.Success);
+            Assert.Equal("Version for none module was modified", response.Errors[0]);
+        }
+    }
+}
