46aee307d0bf0ea0bb0f65c8696f26bb5a15d525
Dominik Foks <dominik.foks@stud.wsi.edu.pl>
2024-07-15 12:45:46 +0200

    feat: Added some integration tests

--- eRM_VersionHub/Tests/Tests/BasicTest.cs
+++ eRM_VersionHub/Tests/Tests/BasicTest.cs
@@ -1,27 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using eRM_VersionHub.Services;
+using eRM_VersionHub.Models;
+using System.Text.Json;
 
 namespace eRM_VersionHub_Tester.Tests
 {
-    public class BasicTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+    public class TextFixture : WebApplicationFactory<Program>
     {
-        private readonly WebApplicationFactory<Program> _factory = factory;
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        { 
+            builder.UseEnvironment("Testing");
+            base.ConfigureWebHost(builder);
+        }
+    }
+
+    public class BasicTests : IClassFixture<TextFixture>
+    {
+        private HttpClient HttpClient { get; }
+
+        public BasicTests(TextFixture fixture)
+        {
+            WebApplicationFactoryClientOptions webAppFactoryClientOptions = new()
+            {
+                AllowAutoRedirect = false
+            };
+            HttpClient = fixture.CreateClient(webAppFactoryClientOptions);
+            Thread.Sleep(200);
+        }
 
         [Theory]
-        [InlineData("")]
+        [InlineData("/User")]
+        public async Task GetUsers(string url)
+        {  
+            HttpResponseMessage response = await HttpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string result = await response.Content.ReadAsStringAsync();
+            List<User>? array = JsonManager.Deserialize<List<User>?>(result);
+            Assert.NotNull(array);
+            Assert.NotEmpty(array);
+        }
 
-        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
-        {
-            // Arrange
-            Uri baseUrl = new("http://localhost:5204");
-            HttpClient client = _factory.CreateDefaultClient(baseUrl);
+        [Theory]
+        [InlineData("/User/admin")]
+        public async Task GetUser(string url)
+        {  
+            HttpResponseMessage response = await HttpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string result = await response.Content.ReadAsStringAsync();
+            User? user = JsonManager.Deserialize<User?>(result);
+            Assert.NotNull(user);
+            Assert.Equal("admin", user.Username);
+        }
 
-            // Act
-            HttpResponseMessage response = await client.GetAsync(url);
-
-            // Assert
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("text/html; charset=utf-8",
-                response.Content.Headers.ContentType.ToString());
+        [Theory]
+        [InlineData("/User/#")]
+        public async Task GetNonExistentUser(string url)
+        {  
+            HttpResponseMessage response = await HttpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string result = await response.Content.ReadAsStringAsync();
+            Action action = new(() =>
+            {
+                User? user = JsonManager.Deserialize<User?>(result);
+            });
+            Assert.Throws<JsonException>(action);
         }
     }
 }
