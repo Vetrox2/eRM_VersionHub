eaeabbc17624f446daa7b198d651b9b7a2a081f3
void0xf <lucyfer99100@gmail.com>
2024-07-23 10:50:49 +0200

    fix; apply changes functions

--- frontend/src/components/modals/default-modal/default-modal.component.ts
+++ frontend/src/components/modals/default-modal/default-modal.component.ts
@@ -2,50 +2,80 @@ import { CommonModule } from '@angular/common';
 import { Output, EventEmitter } from '@angular/core';
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { FormsModule } from '@angular/forms';
-import { TagSelectorComponent } from "../../tag-selector/tag-selector.component";
+import { TagSelectorComponent } from '../../tag-selector/tag-selector.component';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule, MatButtonToggleModule, FormsModule, TagSelectorComponent],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    FormsModule,
+    TagSelectorComponent,
+  ],
   template: `
     <div class="container">
       <h2 mat-dialog-title>{{ data.title }}</h2>
       <mat-dialog-content>{{ data.message }}</mat-dialog-content>
-      <app-tag-selector [selectedOption]="selectedTag" (tagSelected)="onTagSelected($event)"></app-tag-selector>
+      @if(this.showTags){
+      <app-tag-selector
+        [selectedOption]="selectedTag"
+        (tagSelected)="onTagSelected($event)"
+      ></app-tag-selector>
+      }
       <mat-dialog-actions>
         <button mat-button (click)="onCancel()">Cancel</button>
-        <button mat-button (click)="onConfirm()" cdkFocusInitial>Confirm</button>
+        <button mat-button (click)="onConfirm()" cdkFocusInitial>
+          Confirm
+        </button>
       </mat-dialog-actions>
     </div>
-
   `,
-  styles: [`
-    .container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      text-align: center;
-      padding: 20px;
-    }
-    mat-dialog-content { margin-bottom: 20px; }
-    mat-button-toggle-group { margin-bottom: 20px; }
-    mat-dialog-actions { justify-content: center; margin-top: 20px; }
-    button { margin: 0 10px; }
-  `]
+  styles: [
+    `
+      .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        padding: 20px;
+      }
+      mat-dialog-content {
+        margin-bottom: 20px;
+      }
+      mat-button-toggle-group {
+        margin-bottom: 20px;
+      }
+      mat-dialog-actions {
+        justify-content: center;
+        margin-top: 20px;
+      }
+      button {
+        margin: 0 10px;
+      }
+    `,
+  ],
 })
 export class DefaultModalComponent {
   selectedTag: string = 'none';
+  showTags: boolean = true;
 
   constructor(
     public dialogRef: MatDialogRef<DefaultModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.selectedTag = data.selectedTag || 'none';
+    this.showTags = data.showTags;
   }
 
   onTagSelected(tag: string) {
@@ -60,4 +90,3 @@ export class DefaultModalComponent {
     this.dialogRef.close();
   }
 }
-
