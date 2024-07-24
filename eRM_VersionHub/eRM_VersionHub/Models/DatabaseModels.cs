3dba1a8a3b9cbd31a3c752f3c219f9bb986471c8
Dominik Foks <dominik.foks@stud.wsi.edu.pl>
2024-07-24 12:16:57 +0200

    feat: Added FavoriteTests

--- eRM_VersionHub/eRM_VersionHub/Models/DatabaseModels.cs
+++ eRM_VersionHub/eRM_VersionHub/Models/DatabaseModels.cs
@@ -7,6 +7,7 @@ namespace eRM_VersionHub.Models
         public required string Username { get; set; }
         public required string AppID { get; set; }
         public override string ToString() => this.Serialize();
+        public virtual bool Equals(Permission permission) => Username == permission.Username && AppID == permission.AppID;
     }
 
     public class Favorite
@@ -14,6 +15,7 @@ namespace eRM_VersionHub.Models
         public required string Username { get; set; }
         public required string AppID { get; set; }
         public override string ToString() => this.Serialize();
+        public virtual bool Equals(Favorite favorite) => Username == favorite.Username && AppID == favorite.AppID;
     }
 
     public class User
