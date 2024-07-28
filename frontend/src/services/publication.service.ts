4858b64b3ed9075500be456146b8c5d3355be5d5
void0xf <lucyfer99100@gmail.com>
2024-07-28 22:43:38 +0200

    feat: implement publication service

---
+++ frontend/src/services/publication.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Version } from '../models/version.model';
+import { ApiResponse } from '../models/api-response.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PublicationService {
+  constructor(private apiService: ApiService) {}
+
+  publishVersion(version: Version): Observable<ApiResponse<any>> {
+    return this.apiService.post('Publication', [version]);
+  }
+
+  unPublishVersion(version: Version): Observable<ApiResponse<any>> {
+    return this.apiService.delete('Publication', [version]);
+  }
+}
