42cd9ea99d0fd614bea9e77bb3345a42e0c17cd6
void0xf <lucyfer99100@gmail.com>
2024-07-28 22:46:31 +0200

    refactor: create new tests

--- frontend/src/services/app.service.spec.ts
+++ frontend/src/services/app.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { AppService } from './app-service.service';
+import { AppService } from './app.service';
 
 describe('AppServiceService', () => {
   let service: AppService;
