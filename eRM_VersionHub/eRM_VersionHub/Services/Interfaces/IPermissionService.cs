ddc82f8a5077aff348586d162b3b188fd917840c
Vetrox2 <119568016+Vetrox2@users.noreply.github.com>
2024-07-29 11:22:16 +0200

    feat(PermissionService): Added validation of user permissions  to given modules

--- eRM_VersionHub/eRM_VersionHub/Services/Interfaces/IPermissionService.cs
+++ eRM_VersionHub/eRM_VersionHub/Services/Interfaces/IPermissionService.cs
@@ -1,4 +1,5 @@
-﻿using eRM_VersionHub.Models;
+﻿using eRM_VersionHub.Dtos;
+using eRM_VersionHub.Models;
 
 
 namespace eRM_VersionHub.Services.Interfaces
@@ -8,5 +9,6 @@ namespace eRM_VersionHub.Services.Interfaces
         Task<ApiResponse<Permission?>> CreatePermission(Permission permission);
         Task<ApiResponse<List<Permission>>> GetPermissionList(string Username);
         Task<ApiResponse<Permission?>> DeletePermission(Permission permission);
+        Task<bool> ValidatePermissions(VersionDto version, MyAppSettings settings, string? userName);
     }
 }
