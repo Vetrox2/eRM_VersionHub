0c50b129708ce0fc33d372553d1a4fa35164adab
void0xf <lucyfer99100@gmail.com>
2024-07-30 18:33:13 +0200

    test: removed erm dashboard tests

--- frontend/src/pages/erm-dashboard.page/erm-dashboard.page.component.spec.ts
+++ frontend/src/pages/erm-dashboard.page/erm-dashboard.page.component.spec.ts
@@ -1,22 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ErmDashboardPageComponent } from './erm-dashboard.page.component';
+// import { ErmDashboardPageComponent } from './erm-dashboard.page.component';
 
-describe('ErmDashboardPageComponent', () => {
-  let component: ErmDashboardPageComponent;
-  let fixture: ComponentFixture<ErmDashboardPageComponent>;
+// describe('ErmDashboardPageComponent', () => {
+//   let component: ErmDashboardPageComponent;
+//   let fixture: ComponentFixture<ErmDashboardPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ErmDashboardPageComponent],
-    }).compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [ErmDashboardPageComponent],
+//     }).compileComponents();
 
-    fixture = TestBed.createComponent(ErmDashboardPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//     fixture = TestBed.createComponent(ErmDashboardPageComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+// });
