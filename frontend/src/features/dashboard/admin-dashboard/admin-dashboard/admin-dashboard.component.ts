7d0317a442ef3f99645f0a5693d01987e215b8fa
void0xf <lucyfer99100@gmail.com>
2024-07-31 09:48:11 +0200

    feat: implement admin dashboard

---
+++ frontend/src/features/dashboard/admin-dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { UserSidebarComponent } from '../../../../components/sidebar/user-sidebar/user-sidebar/user-sidebar.component';
+import { UserAppPermissionsComponent } from '../../../user-app-permissions/user-app-permissions.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../../services/auth.service';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatMenuModule,
+    UserSidebarComponent,
+    UserAppPermissionsComponent,
+    MatButtonModule,
+    RouterLink,
+  ],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.scss',
+})
+export class AdminDashboardComponent {
+  private authService = inject(AuthService);
+
+  toggleSidebar() {
+    this.isSidebarActive = !this.isSidebarActive;
+  }
+  handleLogout() {
+    this.authService.logout();
+  }
+  isSidebarActive = false;
+}
