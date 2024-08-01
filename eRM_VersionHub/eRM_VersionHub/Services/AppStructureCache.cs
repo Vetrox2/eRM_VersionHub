0bd4d6b4281cae080c02ea475a18ffa839bf012a
Vetrox2 <mateuszmulka@spoko.pl>
2024-08-01 13:11:06 +0200

    feat(Cache): Added service to manage AppStructure in cache

---
+++ eRM_VersionHub/eRM_VersionHub/Services/AppStructureCache.cs
@@ -0,0 +1,28 @@
+﻿using eRM_VersionHub.Dtos;
+using eRM_VersionHub.Services.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace eRM_VersionHub.Services
+{
+    public class AppStructureCache(IMemoryCache memoryCache) : IAppStructureCache
+    {
+        private readonly string key = "appStructure";
+        public Task<List<AppStructureDto>?> GetAppStructureAsync()
+        {
+            memoryCache.TryGetValue(key, out List<AppStructureDto> appStructure);
+            return Task.FromResult(appStructure);
+        }
+
+        public Task SetAppStructureAsync(List<AppStructureDto> appStructure)
+        {
+            memoryCache.Set(key, appStructure, TimeSpan.FromHours(1));
+            return Task.CompletedTask;
+        }
+
+        public Task InvalidateAppStructureAsync()
+        {
+            memoryCache.Remove(key);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ eRM_VersionHub/eRM_VersionHub/Services/Interfaces/IAppStructureCache.cs
@@ -0,0 +1,11 @@
+﻿using eRM_VersionHub.Dtos;
+
+namespace eRM_VersionHub.Services.Interfaces
+{
+    public interface IAppStructureCache
+    {
+        Task<List<AppStructureDto>?> GetAppStructureAsync();
+        Task SetAppStructureAsync(List<AppStructureDto> appStructure);
+        Task InvalidateAppStructureAsync();
+    }
+}
