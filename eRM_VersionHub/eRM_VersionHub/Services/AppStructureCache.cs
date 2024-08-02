4a6fdd155462a22da57f1958c756b6456e974a5e
Vetrox2 <mateuszmulka@spoko.pl>
2024-08-02 21:17:43 +0200

    feat(Cache): added logger to cache

--- eRM_VersionHub/eRM_VersionHub/Services/AppStructureCache.cs
+++ eRM_VersionHub/eRM_VersionHub/Services/AppStructureCache.cs
@@ -1,11 +1,13 @@
 ﻿using eRM_VersionHub.Dtos;
+using eRM_VersionHub.Models;
 using eRM_VersionHub.Services.Interfaces;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace eRM_VersionHub.Services
 {
-    public class AppStructureCache(IMemoryCache memoryCache, IServiceProvider serviceProvider) : IAppStructureCache
+    public class AppStructureCache(IMemoryCache memoryCache, IServiceProvider serviceProvider, ILogger<AppStructureCache> logger) : IAppStructureCache
     {
         private readonly string key = "appStructure";
 
@@ -17,16 +19,20 @@ namespace eRM_VersionHub.Services
 
         public void SetAppStructure(List<AppStructureDto> appStructure)
         {
+            logger.LogDebug(AppLogEvents.Service, "Saving new apps structure into cache");
             memoryCache.Set(key, appStructure, new MemoryCacheEntryOptions{ SlidingExpiration = TimeSpan.FromMinutes(15) });
         }
 
         public void InvalidateAppStructure()
         {
+            logger.LogDebug(AppLogEvents.Service, "Deleting apps structure from cache");
             memoryCache.Remove(key);
         }
 
         public void UpdateModuleStatus(VersionDto version, bool isPublishAction)
         {
+            logger.LogDebug(AppLogEvents.Service, "Updating apps structure in cache, version: {version}", version);
+
             var appStructure = GetAppStructure();
             if (appStructure == null)
             {
