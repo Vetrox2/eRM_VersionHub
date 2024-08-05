5976fc22ab37b581d1787b6adf7fb383b740a95f
void0xf <lucyfer99100@gmail.com>
2024-08-05 03:28:46 +0200

    refactor(userSidebar): adjust datatypes

--- frontend/src/components/sidebar/user-sidebar/user-sidebar/user-sidebar.component.ts
+++ frontend/src/components/sidebar/user-sidebar/user-sidebar/user-sidebar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -8,7 +7,6 @@ import { AsyncPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { AdminService } from '../../../../services/admin.service';
 import { UserSelectionService } from '../../../../services/user-selection.service';
 import { SearchComponent } from '../../../search/search.component';
-import { UserApps } from '../../../../models/user-apps.model';
 
 @Component({
   selector: 'app-user-sidebar',
@@ -28,59 +26,43 @@ import { UserApps } from '../../../../models/user-apps.model';
   styleUrls: ['./user-sidebar.component.scss'],
 })
 export class UserSidebarComponent implements OnInit {
-  users$: Observable<UserApps[]>;
-  filteredUsers$: Observable<UserApps[]>;
-  selectedUser$: Observable<UserApps | null>;
+  users: string[] = [];
+  filteredUsers: string[] = [];
+  selectedUser: string = '';
   loading = true;
   error = '';
 
   constructor(
     private adminService: AdminService,
     private userSelectionService: UserSelectionService
-  ) {
-    this.users$ = this.adminService.users$;
-    this.filteredUsers$ = this.users$;
-    this.selectedUser$ = this.userSelectionService.getSelectedUser();
-  }
+  ) {}
 
   ngOnInit() {
-    this.loadUsers();
-  }
-
-  loadUsers() {
-    this.loading = true;
     this.adminService.loadUsers();
-    this.users$.subscribe(
+    this.adminService.users$.subscribe(
       (users) => {
-        this.loading = false;
-        if (users.length === 0) {
-          this.error = 'No users found.';
+        this.users = users;
+        this.filteredUsers = users;
+        if (users.length > 0) {
+          this.selectUser(users[0]);
         }
-        this.userSelectionService.setSelectedUser(users[0]);
+        this.loading = false;
       },
       (error) => {
+        this.error = 'Error loading users';
         this.loading = false;
-        this.error = 'Error loading users. Please try again.';
-        console.error('Error loading users:', error);
       }
     );
   }
 
   onSearchValueChanged(value: string) {
-    this.filteredUsers$ = this.users$.pipe(
-      map((users) =>
-        users.filter((user) =>
-          user.Username.toLowerCase().includes(value.toLowerCase())
-        )
-      )
+    this.filteredUsers = this.users.filter((user) =>
+      user.toLowerCase().includes(value.toLowerCase())
     );
   }
 
-  selectUser(user: UserApps) {
+  selectUser(user: string) {
+    this.selectedUser = user;
     this.userSelectionService.setSelectedUser(user);
-  }
-
-  trackByUsername(index: number, user: UserApps): string {
-    return user.Username;
   }
 }
