8b801ffe576c516e42f5cfaacf4fea5131255fd7
void0xf <lucyfer99100@gmail.com>
2024-08-07 02:42:54 +0200

    refactor: remove unused functions

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,69 +1,38 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
-import { Router } from '@angular/router';
-import { isPlatformBrowser } from '@angular/common';
 import Keycloak from 'keycloak-js';
-import { initializeKeycloak } from '../keycloak.config';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { KeycloakService } from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private keycloakInstance: Keycloak | null = null;
-  private isInitialized = false;
-  private tokenSubject = new BehaviorSubject<string | undefined>(undefined);
+  // private tokenSubject = new BehaviorSubject<string | undefined>(undefined);
 
-  constructor(
-    private router: Router,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {
-    if (isPlatformBrowser(this.platformId)) {
-      this.keycloakInstance = initializeKeycloak();
-    }
-  }
+  constructor(private readonly keycloak: KeycloakService) {}
 
   isAuthenticated(): boolean {
-    return this.keycloakInstance?.authenticated ?? false;
+    return this.keycloak.isLoggedIn() ?? false;
+  }
+  isLoggedIn(): boolean {
+    return this.keycloak.isLoggedIn();
+  }
+  logout(): void {
+    this.keycloak.logout();
+  }
+  getUserProfile(): any {
+    return this.keycloak.loadUserProfile();
   }
 
   login(): void {
-    this.keycloakInstance?.login();
+    this.keycloak.login();
   }
 
-  logout(): void {
-    this.keycloakInstance?.logout({
-      redirectUri: window.location.origin + '/login',
-    });
-  }
+  // getToken(): string | undefined {
+  //   return this.tokenSubject.value;
+  // }
 
-  getToken(): string | undefined {
-    return this.tokenSubject.value;
-  }
-
-  getToken$(): Observable<string | undefined> {
-    return this.tokenSubject.asObservable();
-  }
-
-  initializeKeycloak(): Promise<boolean> {
-    if (!isPlatformBrowser(this.platformId)) {
-      return Promise.resolve(false);
-    }
-
-    return new Promise((resolve, reject) => {
-      this.keycloakInstance
-        ?.init({ onLoad: 'check-sso' })
-        .then((auth) => {
-          this.isInitialized = true;
-          this.tokenSubject.next(this.keycloakInstance?.token);
-
-          if (!auth) {
-            this.router.navigate(['/login']);
-          } else {
-            this.router.navigate(['/dashboard']);
-          }
-          resolve(auth);
-        })
-        .catch(reject);
-    });
-  }
+  // getToken$(): Observable<string | undefined> {
+  //   return this.tokenSubject.asObservable();
+  // }
 }
