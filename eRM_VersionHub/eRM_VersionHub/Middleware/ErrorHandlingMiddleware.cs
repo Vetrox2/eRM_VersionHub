b277b961e1a6593409e143b69f7a98a13924cfa9
void0xf <lucyfer99100@gmail.com>
2024-08-08 02:08:08 +0200

    feat(Middleware):  implement error handling middleware

---
+++ eRM_VersionHub/eRM_VersionHub/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+using System.Text.Json;
+using eRM_VersionHub.Models;
+
+namespace eRM_VersionHub.Middleware
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message)
+            : base(message) { }
+    }
+
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message)
+            : base(message) { }
+    }
+
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(
+            RequestDelegate request,
+            ILogger<ErrorHandlingMiddleware> logger
+        )
+        {
+            _next = request;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var response = new ApiResponse<object>(["An unexpected error occurred."]);
+
+            switch (exception)
+            {
+                case NotFoundException notFoundException:
+                    code = HttpStatusCode.NotFound;
+                    response =
+                        notFoundException.Message == ""
+                            ? response
+                            : new ApiResponse<object>([notFoundException.Message]);
+                    break;
+                case BadRequestException badRequestException:
+                    code = HttpStatusCode.BadRequest;
+                    response =
+                        badRequestException.Message == ""
+                            ? response
+                            : new ApiResponse<object>([badRequestException.Message]);
+                    break;
+                case InvalidOperationException:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+                default:
+                    _logger.LogError(exception, "An unhandled exception has occurred");
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
